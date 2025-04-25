0e29cb4094464423ff40082d7cd171773e6dac31
Oleg Chemodanov <icas711>
2025-04-25 09:15:07 +0300

    fix secure storage (read data crashed)

--- lib/src/storage/token_storage/token_storage_impl.dart
+++ lib/src/storage/token_storage/token_storage_impl.dart
@@ -1,33 +1,34 @@
-
+import 'package:flutter/services.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 import 'auth_token_pair.dart';
 
-
-
-
 /// {@template tokens_storage.class}
 /// Implementation [TokenStorage]
 /// {@endtemplate}
 final class TokenStorageImplShared implements TokenStorage<AuthTokenPair> {
- /* final SharedPreferences _secureStorage;
+  /* final SharedPreferences _secureStorage;
 
   /// {@macro tokens_storage.class}
   const TokenStorageImplShared(this._secureStorage);*/
 
   @override
   Future<AuthTokenPair?> read() async {
-    final SharedPreferences _secureStorage=await SharedPreferences.getInstance();
-    final tokenJson = _secureStorage.getString(TokensStorageKeys.authToken.keyName);
+    final SharedPreferences _secureStorage =
+        await SharedPreferences.getInstance();
+    final tokenJson = _secureStorage.getString(
+      TokensStorageKeys.authToken.keyName,
+    );
     if (tokenJson == null) return null;
     return AuthTokenPair.fromJson(tokenJson);
   }
 
   @override
   Future<void> write(AuthTokenPair token) async {
-    final SharedPreferences _secureStorage=await SharedPreferences.getInstance();
-     await _secureStorage.setString(
+    final SharedPreferences _secureStorage =
+        await SharedPreferences.getInstance();
+    await _secureStorage.setString(
       TokensStorageKeys.authToken.keyName,
       token.toJson(),
     );
@@ -35,7 +36,8 @@ final class TokenStorageImplShared implements TokenStorage<AuthTokenPair> {
 
   @override
   Future<void> delete() async {
-    final SharedPreferences _secureStorage=await SharedPreferences.getInstance();
+    final SharedPreferences _secureStorage =
+        await SharedPreferences.getInstance();
     for (final key in TokensStorageKeys.values) {
       await _secureStorage.remove(key.keyName);
     }
@@ -50,15 +52,24 @@ final class TokenStorageImpl implements TokenStorage<AuthTokenPair> {
 
   @override
   Future<AuthTokenPair?> read() async {
-    final tokenJson = await _secureStorage.read(key: TokensStorageKeys.authToken.keyName);
-
-    if (tokenJson == null) return null;
-
-    return AuthTokenPair.fromJson(tokenJson);
+    try {
+      final tokenJson = await _secureStorage.read(
+        key: TokensStorageKeys.authToken.keyName,
+      );
+      return tokenJson == null ? null : AuthTokenPair.fromJson(tokenJson);
+    } on PlatformException catch (e) {
+      if (e.code == 'BadPaddingException') {
+        await _secureStorage.delete(
+          key: TokensStorageKeys.authToken.keyName,
+        ); // Удаляем битые данные
+        print('Ошибка дешифровки. Данные удалены.');
+      }
+    }
+    return null;
   }
 
   @override
-  Future<void> write(AuthTokenPair token) async{
+  Future<void> write(AuthTokenPair token) async {
     return await _secureStorage.write(
       key: TokensStorageKeys.authToken.keyName,
       value: token.toJson(),
@@ -83,7 +94,6 @@ enum TokensStorageKeys {
 
   const TokensStorageKeys(this.keyName);
 }
-
 
 abstract class TokenStorage<T> {
   /// Returns the stored token asynchronously.
